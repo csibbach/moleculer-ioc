bfcd3793e8619c85c98ecd11de4f44bc253d8263
csibbach <charlie@hypernetlabs.io>
2021-09-29 16:49:41 -0700

    Bootstrap the package

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Service, 
+    ServiceBroker } from "moleculer";
+
+export class IOCService extends Service {
+    public constructor(public broker: ServiceBroker) {
+		super(broker);
+    }
+}
